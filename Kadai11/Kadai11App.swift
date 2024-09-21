cbb47df4dafcd2968fa542ec2da1d192cc608447
AY <yugechan666@gmail.com>
2024-09-21 14:02:24 +0900

    Initial Commit

---
+++ Kadai11/Kadai11App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai11App.swift
+//  Kadai11
+//
+//  Created by nobu0621 on 2024/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Kadai11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
